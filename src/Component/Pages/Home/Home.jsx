84f8bf51c9806711dd77ee4a319c62ecebc91038
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2023-10-23 16:04:25 +0600

    counter and modal added

--- src/Component/Pages/Home/Home.jsx
+++ src/Component/Pages/Home/Home.jsx
@@ -1,38 +1,66 @@
-import React, { useEffect, useState } from 'react';
-import Navbar from '../../Shared/Navbar/Navbar';
-import SingleCard from '../../SingleCard/SingleCard';
+import React, { useEffect, useState } from "react";
+import Modal from "react-modal";
+import Navbar from "../../Shared/Navbar/Navbar";
+import SingleCard from "../../SingleCard/SingleCard";
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+Modal.setAppElement("#root");
 
 const Home = () => {
-    const [cards, setCards] = useState([]);
-    const [cart, setCart] = useState([]);
+  const [cards, setCards] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [modalIsOpen, setIsOpen] = useState(false);
 
-    const addToCart = (props) => {
-        const newCart = [...cart, props]
-        setCart(newCart)
-    }
+  function openModal() {
+    setIsOpen(true);
+  }
 
-    useEffect(() => {
-        fetch("data.json")
-        .then(res => res.json())
-        .then(data => setCards(data));
-    }, [])
-    return (
-        <div>
-            <Navbar cart = {cart}/>
-            <div className="allCards">
-                <div className="card-container">
-                    {
-                        cards.map(card => <SingleCard
-                        key={card.id}
-                        SingleCard = {card}
-                        addToCart = {addToCart}
-                        /> )
-                    }
-                </div>
-            </div>
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  const addToCart = (props) => {
+    const newCart = [...cart, props];
+    setCart(newCart);
+  };
+
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setCards(data));
+  }, []);
+  return (
+    <div>
+      <Navbar cart={cart} />
+      <button onClick={openModal}>Open Modal</button>
+      <div className="allCards">
+        <div className="card-container">
+          {cards.map((card) => (
+            <SingleCard key={card.id} SingleCard={card} addToCart={addToCart} />
+          ))}
         </div>
-    );
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+        
+      >
+        <button onClick={closeModal} className="bg-red-500 text-white px-[6px] rounded-full " >X</button>
+        <h1>hi i am mridul</h1>
+      </Modal>
+    </div>
+  );
 };
 
 export default Home;
--- src/Component/Shared/Navbar/Navbar.jsx
+++ src/Component/Shared/Navbar/Navbar.jsx
@@ -6,7 +6,8 @@ const Navbar = ({cart}) => {
         <header className='bg-gray-500 py-4 sticky top-0 z-20'>
             <div className="child-div text-white  flex justify-between container mx-auto">
             <div className="about"><h1 className='text-2xl'>Kopa Samsu Store</h1></div>
-            <div className="icon my-auto text-xl relative"> <button><BsFillCartFill /> <sup className='absolute right-'>{cart.length}</sup></button> </div>
+            <span className='absolute right-6 top-2 bg-red-600 px-[6px] rounded-full'>  {cart.length}</span>
+            <div className="icon my-auto text-xl relative"> <button><BsFillCartFill /></button> </div>
             </div>
         </header>
     );
